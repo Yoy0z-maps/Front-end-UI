6fbcb391b7446c26fd6f991f58afbd8655da98a7
Yoy0z-maps <165480026+Yoy0z-maps@users.noreply.github.com>
2024-07-01 22:51:13 +0800

    add module.css, fix vercel release error

--- src/app/isometric/page.tsx
+++ src/app/isometric/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import styles from "./styles.module.css";
 
 export default function IsoMatric() {
   return (
@@ -44,7 +45,9 @@ export default function IsoMatric() {
             Our Team
           </Link>
         </li>
-        <li className="relative list-none w-[200px] bg-slate-600 p-[15px] before:content-[''] before:absolute before:top-[-20px] before:w-[40px] before:h-full before:bg-slate-700 before:left-[-40px] before:skew-y-[45deg] after:content-[''] after:absolute after:top-[-40px] after:left-0 after:w-full after:h-[40px] after:bg-slate-600 after:skew-x-[45deg] after:origin-bottom z-0 hover:bg-isobg hover:translate-x-[50px] hover:before:bg-isohoverbefore hover:after:bg-isohoverafter transition duration-500 shadow last:after:shadow-md">
+        <li
+          className={`relative list-none w-[200px] bg-slate-600 p-[15px] before:content-[''] before:absolute before:top-[-20px] before:w-[40px] before:h-full before:bg-slate-700 before:left-[-40px] before:skew-y-[45deg] after:content-[''] after:absolute after:top-[-40px] after:left-0 after:w-full after:h-[40px] after:bg-slate-600 after:skew-x-[45deg] after:origin-bottom z-0 hover:bg-isobg hover:translate-x-[50px] hover:before:bg-isohoverbefore hover:after:bg-isohoverafter transition duration-500 ${styles.customLastChild}`}
+        >
           <Link
             href="#"
             className="block uppercase text-gray-500 tracking-tighter hover:text-gray-100 transition-all"
